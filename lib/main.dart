5970fe24241172d7f93d6bb3636dab2abac1367f
Minh Pham <43801761+phamduyanminh@users.noreply.github.com>
2022-07-08 02:06:10 -0400

    Add font style

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
         accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
       ),
       home: ProductsOverviewScreen(),
     );
